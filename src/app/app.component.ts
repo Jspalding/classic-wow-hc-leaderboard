1ad6719614f82cb5e8f225932ead7777aa98429f
James Spalding <jamescodebase@outlook.com>
2024-11-04 11:02:48 +0000

    Cleaned console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit {
                     this.hasAuthToken = true;
                 });
         } else {
-            console.log('TOKEN ACTIVE', this.authService.getToken());
+            console.log('TOKEN ACTIVE');
             this.hasAuthToken = true;
         }
     }
