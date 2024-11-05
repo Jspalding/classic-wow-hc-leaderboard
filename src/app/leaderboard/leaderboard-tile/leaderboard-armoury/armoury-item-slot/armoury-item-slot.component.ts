092e56bddda4ea92b05a0d85731522d8df23bb1c
James Spalding <jamescodebase@outlook.com>
2024-11-05 12:21:54 +0000

    Added icon loading state + fixed equipment responsiveness

--- src/app/leaderboard/leaderboard-tile/leaderboard-armoury/armoury-item-slot/armoury-item-slot.component.ts
+++ src/app/leaderboard/leaderboard-tile/leaderboard-armoury/armoury-item-slot/armoury-item-slot.component.ts
@@ -4,11 +4,12 @@ import { EquippedItem } from '../../../../core/models/character/character-gear.i
 import { ItemService } from '../../../../core/services/item.service';
 import { take } from 'rxjs';
 import { ItemIconMedia } from '../../../../core/models/item/item-media.interface';
+import { InlineLoaderComponent } from '../../../../shared/inline-loader/inline-loader.component';
 
 @Component({
     selector: 'armoury-item-slot',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, InlineLoaderComponent],
     templateUrl: './armoury-item-slot.component.html',
     styleUrl: './armoury-item-slot.component.scss',
 })
@@ -19,14 +20,19 @@ export class ArmouryItemSlotComponent implements OnInit {
     iconSrc: string = '';
     wowHeadTooltip: string = '';
 
-    ngOnInit(): void {
-        this.wowHeadTooltip = 'item=' + this.item.item.id + '&domain=classic';
+    isLoading: boolean = false;
 
+    ngOnInit(): void {
+        this.isLoading = true;
+
+        this.wowHeadTooltip = 'item=' + this.item.item.id + '&domain=classic';
         this.itemService
             .getItemMediaByUrl(this.item.media.key.href)
             .pipe(take(1))
             .subscribe((response: ItemIconMedia) => {
                 this.iconSrc = response.assets[0].value;
+
+                this.isLoading = false;
             });
     }
 }
