5ae06693af5df2da5ef410d097fbe0fbc98c50d1
James Spalding <Jspalding@users.noreply.github.com>
2024-11-11 20:08:41 +0000

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ export const environment = {
     euApiUrl: 'https://eu.api.blizzard.com',
     authUrl: 'https://oauth.battle.net/token',
     euProfileNamespace: 'profile-classic1x-eu',
-    clientId: '84deb614d11f4955b130beee51e3f9a6',
-    clientSecret: 'LUv282pyF9fTiyUvtLjIiq7cqFVZMg03',
+    clientId: '',
+    clientSecret: '',
 };
