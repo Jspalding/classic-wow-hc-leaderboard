fca19b91d761adb5c351f72697612e10ffe57619
James Spalding <jamescodebase@outlook.com>
2024-11-11 20:15:38 +0000

    Remove environment files from tracking

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    euApiUrl: 'https://eu.api.blizzard.com',
-    authUrl: 'https://oauth.battle.net/token',
-    euProfileNamespace: 'profile-classic1x-eu',
-    clientId: '',
-    clientSecret: '',
-};
