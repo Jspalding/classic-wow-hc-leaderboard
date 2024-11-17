85480169754ed9437d5efa5d3cf8540e5893134a
James Spalding <jamescodebase@outlook.com>
2024-11-17 22:47:57 +0000

    Fixing pipeline

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    euApiUrl: 'https://eu.api.blizzard.com',
+    authUrl: 'https://oauth.battle.net/token',
+    euProfileNamespace: 'profile-classic1x-eu',
+    clientId: '',
+    clientSecret: '',
+};
